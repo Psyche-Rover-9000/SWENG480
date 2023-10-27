a0172268ba075370dfe1afd5b47fce07ac862f6d
Jacob-Meyer66 <146495160+Jacob-Meyer66@users.noreply.github.com>
2023-10-27 18:31:16 -0500

    I have finished the score system.
    both the score system and score board are done.

---
+++ Assets/Score/ScoreBoard.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Author: Jacob Meyer
+ * 
+ * This script is for the Score Board UI element. it should be placed on the element of the same name.
+ * this will allow the score to be counted and, when modifyed, it will update the UI element.
+ * 
+ * Created 10/27/2023
+ * 
+ * changelog: none yet.
+ */
+public class ScoreBoard : MonoBehaviour
+{
+    private int scoreValue = 0;             //the total score of the player. do not mess with this outside this program.
+
+    public SpriteRenderer[] place;          //the location that will be changed. 0 is ones 1 is tens... 6 is millions
+    public Sprite[] digits;                 //the diget that will be displayed. this is maped 1 to 1.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        updateScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //This method will update the score element of the UI
+    void updateScore()
+    {
+        int score = scoreValue;     //this will hold the temporary score to be checked.
+
+        for (int i = 0; i < place.Length; i++) 
+        {
+            int temporaryInt = score % 10;      //the the lowest place and save it.
+            place[i].sprite = digits[temporaryInt];
+            score = score / 10;
+        }
+    }
+
+    //This method will add the given int to the score and then update the UI. Use this method to add or subtract score.
+    void adjustScore(int valueToAdd)
+    {
+        scoreValue += valueToAdd;
+        updateScore();
+    }
+}
