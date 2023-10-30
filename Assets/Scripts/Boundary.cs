f16e7c9409c4aa6e9905c7af6f0fa01d77a4084f
mstanley96 <148163304+mstanley96@users.noreply.github.com>
2023-10-30 19:10:37 -0400

    Level update

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour
+{
+    private Vector2 screenBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 viewPOS = transform.position;
+        viewPOS.x = Mathf.Clamp(viewPOS.x, screenBounds.x, screenBounds.x * -1);
+        viewPOS.y = Mathf.Clamp(viewPOS.y, screenBounds.y, screenBounds.y * -1);
+        transform.position = viewPOS;
+    }
+}
