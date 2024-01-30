d24fa5096c09fb5f3aa12fc03397d17e44eb65ce
leeamb000 <113486016+leeamb000@users.noreply.github.com>
2024-01-29 20:00:47 -0500

    Connected MainWorld and CaveScene
    Rover, pause menu, settings menu, and game music are DontDestroyOnLoad objects

--- Assets/Music/SoundManager.cs
+++ Assets/Music/SoundManager.cs
@@ -7,6 +7,20 @@ public class Music : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
 
+    //creates an instance of the game sound
+    private static Music musicInstance = null;
+    private void Awake()
+    {
+        if (musicInstance == null)
+        {
+            musicInstance = this; //set instance
+            DontDestroyOnLoad(this.gameObject); //used to keep the same object in all scenes
+            return;
+        }
+        Destroy(this.gameObject); //destroy duplicate game music objects when returning to MainWorld
+
+    }
+
     public void MusicOn()
     {
         audioMixer.SetFloat("MusicVolume", 0);
