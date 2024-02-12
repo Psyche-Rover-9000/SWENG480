84a814322e67d99db5f0e53c6935a22eb1aa6337
mstanley96 <148163304+mstanley96@users.noreply.github.com>
2024-02-12 18:52:14 -0500

    Sign Update

--- Assets/Sign.cs
+++ Assets/Sign.cs
@@ -7,7 +7,7 @@ public class Sign : MonoBehaviour
     public GameObject dialogBox;
     public Text dialogText;
     public string dialog;
-    public bool dialogActive;
+    public bool playerInRange;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +17,33 @@ public class Sign : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.E) && playerInRange)
+        {
+            if(dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
-            Debug.Log("Player in range");
+            playerInRange = true;
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
-            Debug.Log("Player left range");
+            playerInRange = false;
+            dialogBox.SetActive(false);
         }
     }
 }
