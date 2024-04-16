be0f73ea466bc6b4d422ab4ceeb14dce7bea04a4
reidrm <113366533+reidrm@users.noreply.github.com>
2024-04-16 13:35:28 -0400

    build 1.0

--- Assets/Scripts/SetSpawnPoint.cs
+++ Assets/Scripts/SetSpawnPoint.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.FilePathAttribute;
 
 //script used to set spawn point when entering a new scene
 public class SetSpawnPoint : MonoBehaviour
